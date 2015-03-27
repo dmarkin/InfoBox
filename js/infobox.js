7460cadecebf87607050467a0a884ddc2c1b5d5e
dreamonus <dmarkin@ua.fm>
2015-03-27 20:29:25 +0200

    Refactoring

--- js/infobox.js
+++ js/infobox.js
@@ -4,11 +4,9 @@
  3. In the /img it's not included images for big find button - I have created them
  4. It is not clear how to use dottedHozLine.png
  5. It is possible, but ineffective to create all DOM markup with JS/jQuery
- 6. It is possible to create and restyle scrollbar for detailed info - if thats need
  */
 
 jQuery(document).ready(
-
     function () {
         var urlRequest = "src/info_box.json",
             currentProduct = 0,
@@ -23,45 +21,55 @@ jQuery(document).ready(
             data: "string",
             dataType: "json",
             mimeType: "application/json",   //avoid FF syntax error
-            cache: false   //avoid IE requests caching problems
+            cache: true   // false avoid IE requests caching problems?
         })
             .success(function (response) {
                 var productInfoString = JSON.stringify(response);
                 productInfo = JSON.parse(productInfoString);
                 numOfProducts = productInfo.length;
                 fillInfobox(productInfo[currentProduct]);
+                eventsInfobox();
             })
             .error(function (errorMessage) {
-                $('h2').html(errorMessage.statusText);
+                $('h2').text(errorMessage.statusText);
             });
 
         // filling Infobox
         function fillInfobox(obj) {
             var $descr = $('.ib-description');
-            if ($descr.hasClass('ib-show-details')) {
-                $descr.removeClass('ib-show-details').addClass('ib-hide-details');
-                $('.ib-details').html('show details');
+            var $details = $('.ib-details');
+
+            // clear details block
+            if (!$descr.hasClass('ib-hiding-details')) {
+                $descr.animate({'height': '35px'}, 0).removeClass('ib-scroll').addClass('ib-hiding-details');
             }
 
-            $('h2').html(obj.title);
-            $('.ib-description-text').html(obj.description);
-            $('.ib-notes-text').html(obj.note);
-            $('.ib-product-info').hide().fadeIn(ANIMATION_TIME);
-            $('.ib-product-photo').html('<img src="img/' + obj.img + '" title="' + obj.title + '" />').hide().fadeIn(ANIMATION_TIME);
+            var sourceImage = '<img src="img/' + obj.img + '" title="' + obj.title + '" />';
+            $('h2').text(obj.title).hide().fadeIn(ANIMATION_TIME);
+            $('.ib-description-text').text(obj.description).hide().fadeIn(ANIMATION_TIME);
+            $('.ib-notes-text').text(obj.note);
+            $details.text('show details').hide().fadeIn(ANIMATION_TIME);
+
+            // .hide() in this case is easier because we don't need add and remove CSS class {display: none;} with many check-outs
+            $('.ib-product-photo').html(sourceImage).hide().fadeIn(ANIMATION_TIME);
         }
 
         // add events listeners and call function
-        (function eventsInfobox() {
+        function eventsInfobox() {
             $('.ib-left-button')
                 .on('mouseenter', function () {
-                    $(this).css({'backgroundImage': 'url("img/button_bg_orange_left.png")', 'cursor': 'pointer'})
-                        .find('.gray-arrow-left').attr('src', 'img/btn_ic_brn_left.png')
-                        .end().find('.gray-button-label-left').css('color', '#FFF');
+                    $(this).removeClass('sprite-button_bg_white_left')
+                        .addClass('ib-active sprite-button_bg_orange_left')
+                        .find('.sprite-btn_ic_gray_left')
+                        .removeClass('sprite-btn_ic_gray_left').addClass('sprite-btn_ic_brn_left')
+                        .end().find('.gray-button-label-left').addClass('white');
                 })
                 .on('mouseleave', function () {
-                    $(this).css('backgroundImage', 'url("img/button_bg_white_left.png")')
-                        .find('.gray-arrow-left').attr('src', 'img/btn_ic_gray_left.png')
-                        .end().find('.gray-button-label-left').css('color', '#a1a1a1');
+                    $(this).removeClass('ib-active sprite-button_bg_orange_left')
+                        .addClass('sprite-button_bg_white_left')
+                        .find('.sprite-btn_ic_brn_left')
+                        .removeClass('sprite-btn_ic_brn_left').addClass('sprite-btn_ic_gray_left')
+                        .end().find('.gray-button-label-left').removeClass('white');
                 })
                 .on('click', function () {
                     if (currentProduct > 0) {
@@ -75,14 +83,18 @@ jQuery(document).ready(
 
             $('.ib-right-button')
                 .on('mouseenter', function () {
-                    $(this).css({'backgroundImage': 'url("img/button_bg_orange_right.png")', 'cursor': 'pointer'})
-                        .find('.gray-arrow-right').attr('src', 'img/btn_ic_brn_right.png')
-                        .end().find('.gray-button-label-right').css('color', '#FFF');
+                    $(this).removeClass('sprite-button_bg_white_right')
+                        .addClass('ib-active sprite-button_bg_orange_right')
+                        .find('.sprite-btn_ic_gray_right')
+                        .removeClass('sprite-btn_ic_gray_right').addClass('sprite-btn_ic_brn_right')
+                        .end().find('.gray-button-label-right').addClass('white');
                 })
                 .on('mouseleave', function () {
-                    $(this).css('backgroundImage', 'url("img/button_bg_white_right.png")')
-                        .find('.gray-arrow-right').attr('src', 'img/btn_ic_gray_right.png')
-                        .end().find('.gray-button-label-right').css('color', '#a1a1a1');
+                    $(this).removeClass('ib-active sprite-button_bg_orange_right')
+                        .addClass('sprite-button_bg_white_right')
+                        .find('.sprite-btn_ic_brn_right')
+                        .removeClass('sprite-btn_ic_brn_right').addClass('sprite-btn_ic_gray_right')
+                        .end().find('.gray-button-label-right').removeClass('white');
                 })
                 .on('click', function () {
                     if (currentProduct < numOfProducts - 1) {
@@ -96,29 +108,33 @@ jQuery(document).ready(
 
             $('.ib-find-button')
                 .on('mouseenter', function () {
-                    $(this).css({'backgroundImage': 'url("img/button_find_orange_right.png")', 'cursor': 'pointer'})
-                        .find('.orange-button-label-right').css('color', '#FFF');
+                    $(this).removeClass('sprite-button_find_white_right')
+                        .addClass('ib-active sprite-button_find_orange_right')
+                        .find('.orange-button-label-right').addClass('white');
                 })
                 .on('mouseleave', function () {
-                    $(this).css('backgroundImage', 'url("img/button_find_white_right.png")')
-                        .find('.orange-button-label-right').css('color', '#ed6c20');
+                    $(this).removeClass('ib-active sprite-button_find_orange_right')
+                        .addClass('sprite-button_find_white_right')
+                        .find('.orange-button-label-right').removeClass('white');
                 });
 
             $('.ib-details').on('click', function () {
                 var $descr = $('.ib-description');
                 var $photo = $('.ib-product-photo');
                 var $details = $('.ib-details');
-                if ($descr.hasClass('ib-hide-details')) {
+                if ($descr.hasClass('ib-hiding-details')) {
                     $photo.fadeOut(ANIMATION_TIME, function () {
-                        $details.html('hide details');
-                        $descr.removeClass('ib-hide-details').addClass('ib-show-details');
+                        $descr.animate({'height': '235px'}, ANIMATION_TIME, function () {
+                            $details.text('hide details');
+                            $descr.removeClass('ib-hiding-details').addClass('ib-scroll');
+                        });
                     });
                 }
                 else {
                     $photo.fadeIn(ANIMATION_TIME);
-                    $details.html('show details');
-                    $descr.removeClass('ib-show-details').addClass('ib-hide-details');
+                    $descr.animate({'height': '35px'}, 0).removeClass('ib-scroll').addClass('ib-hiding-details');
+                    $details.text('show details');
                 }
             });
-        })();
+        }
     });
